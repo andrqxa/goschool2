2d5c49f716ebdea0629b240b1f7bc515458ed3b6
Andrii Puhachenko <andrqxa@gmail.com>
2021-12-25 12:13:03 +0200

    Move main to cmd

---
+++ cmd/goschool/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"goschool/helpers"
+	"goschool/internal/app/apiserver"
+
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	helpers.LogFatal(err)
+
+	s := apiserver.New(config)
+	helpers.LogFatal(s.Start())
+}
